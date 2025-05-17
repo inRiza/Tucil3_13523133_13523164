b1d15820f1e8881c0eeb23f7f4e6283d35c0048d
MRizainF <icon.firdaus@gmail.com>
2025-05-17 16:22:36 +0700

    feat: add run script

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Compiling Java files..."
+mkdir -p bin
+javac -d bin src/Main.java src/core/*.java src/io/*.java src/algorithms/*.java src/gui/*.java
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed!"
+    exit 1
+fi
+
+echo
+echo "Compilation successful!"
+echo
+echo "Running Rush Hour Puzzle Solver..."
+echo
+java -cp bin gui.MainWindow 
