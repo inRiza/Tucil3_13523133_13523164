c279955789ffeeeaed40755c493b034bd9f4c0d3
MRizainF <icon.firdaus@gmail.com>
2025-05-17 16:17:41 +0700

    chore: commented main (not yet defined)

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,46 @@
-import io.InputHandler;
-import io.Step;
-import core.Board;
-import core.Piece;
-import algorithms.UCS;
-import java.util.List;
+// import io.InputHandler;
+// import io.Step;
+// import core.Board;
+// import core.Piece;
+// import algorithms.UCS;
+// import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            // Read input file
-            System.out.println("Reading input file (test1.txt):");
-            InputHandler inputHandler = new InputHandler();
-            Board board = inputHandler.readInput("test/input/test1.txt");
+// public class Main {
+//     public static void main(String[] args) {
+//         try {
+//             // Read input file
+//             System.out.println("Reading input file (test1.txt):");
+//             InputHandler inputHandler = new InputHandler();
+//             Board board = inputHandler.readInput("test/input/test3.txt");
 
-            // Print initial board information
-            System.out.println("\nBoard dimensions: " + board.getRows() + "x" + board.getCols());
-            System.out.println("Exit position (K): Row " + board.getExitRow() + ", Col " + board.getExitCol());
+//             // Print initial board information
+//             System.out.println("\nBoard dimensions: " + board.getRows() + "x" + board.getCols());
+//             System.out.println("Exit position (K): Row " + board.getExitRow() + ", Col " + board.getExitCol());
 
-            // Print all pieces
-            System.out.println("\nPieces found:");
-            for (Piece piece : board.getPieces()) {
-                System.out.println("Piece ID: " + piece.getId() +
-                        ", Position: (" + piece.getRow() + "," + piece.getCol() + ")" +
-                        ", Size: " + piece.getSize() +
-                        ", Orientation: " + piece.getOrientation() +
-                        ", Is Primary: " + piece.isPrimary());
-            }
+//             // Print all pieces
+//             System.out.println("\nPieces found:");
+//             for (Piece piece : board.getPieces()) {
+//                 System.out.println("Piece ID: " + piece.getId() +
+//                         ", Position: (" + piece.getRow() + "," + piece.getCol() + ")" +
+//                         ", Size: " + piece.getSize() +
+//                         ", Orientation: " + piece.getOrientation() +
+//                         ", Is Primary: " + piece.isPrimary());
+//             }
 
-            // Solve using UCS
-            System.out.println("\nSolving puzzle using UCS...");
-            List<Step> solution = UCS.solve(board);
+//             // Solve using UCS
+//             System.out.println("\nSolving puzzle using UCS...");
+//             List<Step> solution = UCS.solve(board);
 
-            if (solution != null) {
-                System.out.println("\nSolution found! Total steps: " + (solution.size() - 1));
-                Step.printSteps(solution);
-            } else {
-                System.out.println("\nNo solution found!");
-            }
+//             if (solution != null) {
+//                 System.out.println("\nSolution found! Total steps: " + (solution.size() - 1));
+//                 Step.printSteps(solution);
+//             } else {
+//                 System.out.println("\nNo solution found!");
+//             }
 
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-}
+//         } catch (Exception e) {
+//             System.out.println("Error: " + e.getMessage());
+//             e.printStackTrace();
+//         }
+//     }
+// }
